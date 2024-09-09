34462da0cfe63e78bd3f05871a00ad5793a847dc
Robert05-stack <johnrobertabaya@gmail.com>
2024-09-09 20:01:00 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,12 @@
+let name = 'Abaya, John Robert V..';
+
+let WhatDoYouWannaBeComeInYourLife = 'Programmer';
+
+let gender = 'male';
+
+let TwitterHandle = '@Robert698612901';
+
+console.log('What is your Name? ', name);
+console.log('What do you wanna become in your life? ', WhatDoYouWannaBeComeInYourLife);
+console.log('Are you male or female? ', gender);
+console.log('My twitter: ', TwitterHandle);
